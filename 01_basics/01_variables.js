9b751b88faab5641acc206341ec9496318bfbcd9
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2025-01-03 11:08:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accounteEmail ="sanskriti213sm@gmail.com"
+var accountpassword ="12345"
+accountCity ="jaipur"
+let accountstate;
+
+console.table([accountId ,accounteEmail ,accountpassword ,accountCity ,accountstate])
+
+// never use var 
