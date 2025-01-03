ed6cd016a6216c49df822fd814ce15cfd728bb0f
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2025-01-03 11:21:06 +0000

    type conversion

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,26 @@
+// let score ="33abc"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN
+
+// "33"-33
+// "33abc"-NaN
+// true -1 ,false -0
+// null -0
+
+let loggedIn =1
+
+let booleanloggedIn = Boolean(loggedIn)
+console.log(typeof loggedIn);
+console.log(typeof booleanloggedIn)
+console.log(booleanloggedIn)
+
+
+// string type conversion 
+let somenumber=33
+let stringnumber =String(somenumber)
+
+console.log(typeof stringnumber)
