6324c4ad09c33b69e5750beda37e6ce0256b17d2
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2025-01-04 07:58:07 +0000

    date and time

---
+++ 01_basics/05_date_time.js
@@ -0,0 +1,16 @@
+let mydate = new Date();
+
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toTimeString());
+
+
+console.log(typeof mydate);
+
+let mycreateddate =new Date(2023,0,23)
+console.log(mycreateddate.toDateString());
