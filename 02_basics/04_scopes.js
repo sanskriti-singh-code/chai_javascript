914228d81bb77b6a4be2078bb3c4988c2ede2d2d
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2025-01-05 10:14:37 +0000

    scopes

---
+++ 02_basics/04_scopes.js
@@ -0,0 +1,26 @@
+let a = 300
+if(true){
+    let a =10
+    const b =20
+    // console.log("inner: ", a);
+}
+
+// console.log(a)
+
+// for (let i=0; i< Array.length ; i++){
+//          const element =Array[i];
+// } 
+
+// intresting 
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+// console.log(addtwo(5))
+
+const addtwo =function(num){
+    return num+2
+}
