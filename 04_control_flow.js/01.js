6a728aff908334d29333ae17cd53b4bb9b246880
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2025-01-06 08:48:24 +0000

    control flow

---
+++ 04_control_flow.js/01.js
@@ -0,0 +1,33 @@
+// if 
+
+const isuserloggedin =true
+
+// if(2 == "2"){
+//    console.log("executed");
+// }
+
+if(2 === "2"){
+    console.log("executed");
+ }
+ else{
+ console.log("not ")}
+
+ 
+//   < , > ,<= ,>= ,== ,!= ,=== ,!==
+
+
+//  if , else if ,else if ,else ...
+
+// && -AND  ,||- OR
+
+// nullish coalescing operator (??): null undefined 
+
+val1 =null ?? 10
+val2 =undefined ?? 20
+// console.log(val1);
+// console.log(val2);
+
+// ternary operator 
+
+const iceprice =100
+iceprice >100 ? console.log("greater than 100") : console.log("less than equal to ")
---
+++ 04_control_flow.js/02_switchcase.js
@@ -0,0 +1,20 @@
+// switch(key){
+//     case value:
+
+//         break;
+//     case value2:
+//         break;
+
+//     default:
+//         break;
+
+// }
+
+
+// shift + alt +down arrow == to copy selected code downwards 
+
+// falsy value 
+// false,0,-0,BigInt  0n,"" ,null ,undefined ,NaN
+
+// truthy values surprising -otherwise all others are also truthy values 
+//  "0", 'false' ," " ,[] ,{}, function(){} 
